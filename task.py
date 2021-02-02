3a078f2106b34eed081e1e474be05fb3d1ffc22a
Nikhil D R <nikhildr22@gmail.com>
2021-02-02 20:33:55 +0530

    formatting

--- task.py
+++ task.py
@@ -1,15 +1,15 @@
 
-if __name__=="__main__":
+if __name__ == "__main__":
     with open('airlines.csv') as f_hand:
         lis = f_hand.readlines()
     air_count = dict()
     for i in lis[1:]:
         temp = i.split(',')
-        air_count[temp[1]+temp[2]] = air_count.get(temp[1]+temp[2],0) + 1
-    outputs = {'output1':air_count}
+        air_count[temp[1]+temp[2]] = air_count.get(temp[1]+temp[2], 0) + 1
+    outputs = {'output1': air_count}
     maxi = []
     for key, value in air_count.items():
-        maxi.append((value,key))
+        maxi.append((value, key))
     maxi.sort(reverse=True)
     outputs["output2"] = maxi[0]
     outputs["output3"] = maxi[-1]
